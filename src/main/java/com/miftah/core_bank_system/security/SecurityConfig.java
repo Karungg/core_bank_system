94f2d5339fa812494da7e6b4954d40232fe1f879
Karungg <miftahfadilah71@gmail.com>
2026-04-14 09:21:35 +0700

    refactor(security): update security configuration for new endpoints

--- src/main/java/com/miftah/core_bank_system/security/SecurityConfig.java
+++ src/main/java/com/miftah/core_bank_system/security/SecurityConfig.java
@@ -25,7 +25,8 @@ public class SecurityConfig {
         };
 
         private static final String[] USER_WHITELIST = {
-                "/api/accounts/me",
+                "/api/v1/accounts/me",
+                "/api/v1/accounts/me/**"
         };
 
         public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
@@ -40,16 +41,16 @@ public class SecurityConfig {
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers(SWAGGER_WHITELIST).permitAll()
 
-                                                .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/refresh", "/api/auth/logout").permitAll()
-                                                .requestMatchers("/api/users/**").hasRole("ADMIN")
-                                                .requestMatchers("/api/profiles/**").hasAnyRole("ADMIN", "USER")
+                                                .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/logout").permitAll()
+                                                .requestMatchers("/api/v1/users/**").hasRole("ADMIN")
+                                                .requestMatchers("/api/v1/profiles/**").hasAnyRole("ADMIN", "USER")
 
                                                 .requestMatchers(USER_WHITELIST).hasRole("USER")
 
-                                                .requestMatchers("/api/transactions/me", "/api/audits/me").hasRole("USER")
+                                                .requestMatchers("/api/v1/transactions/me", "/api/v1/audits/me", "/api/v1/notifications/**").hasRole("USER")
 
-                                                .requestMatchers("/api/accounts/**", "/api/audits/**").hasRole("ADMIN")
-                                                .requestMatchers("/api/transactions/**").hasAnyRole("ADMIN", "USER")
+                                                .requestMatchers("/api/v1/accounts/**", "/api/v1/audits/**").hasRole("ADMIN")
+                                                .requestMatchers("/api/v1/transactions/**").hasAnyRole("ADMIN", "USER")
                                                 .anyRequest().authenticated())
                                 .sessionManagement(session -> session
                                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
