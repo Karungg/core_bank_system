29334a0761d638b7c900def59a199e420819e773
Karungg <miftahfadilah71@gmail.com>
2026-04-14 09:20:51 +0700

    feat(profile): implement user profile management API

--- src/main/java/com/miftah/core_bank_system/profile/ProfileController.java
+++ src/main/java/com/miftah/core_bank_system/profile/ProfileController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/profiles")
+@RequestMapping("/api/v1/profiles")
 @RequiredArgsConstructor
 public class ProfileController {
 
